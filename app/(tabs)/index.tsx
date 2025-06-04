58341152199a5fc67a6df837a972a005ba7fd4e4
Lachlan Carney <50476416+lachlancarneyapps@users.noreply.github.com>
2025-06-04 21:14:08 +1000

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -340,7 +340,7 @@ export default function TallyTracker() {
 
             <View style={[
               styles.progressBarContainer,
-              playerCount === 2 && { marginBottom: 40, paddingBottom: 40  }
+              playerCount === 2 && { marginBottom: 40,  }
             ]}>
               <LinearGradient
                 colors={[lighterPrimary, lighterSecondary]}
