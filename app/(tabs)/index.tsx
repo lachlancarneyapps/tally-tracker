192fac11dc145af103b20eb9ecc5ae62ef202a70
Lachlan Carney <50476416+lachlancarneyapps@users.noreply.github.com>
2025-06-04 21:16:44 +1000

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -312,7 +312,7 @@ export default function TallyTracker() {
           ]}>
             <View style={[
   styles.totalBadge,
-  playerCount === 2 && { marginTop: 10, marginBottom: 10 }
+  playerCount === 2 && { marginTop: 20, marginBottom: 10 }
 ]}>
 
               <Text style={[
